89bf8aa45e7c979af1dd472c248c0b49ec2f14b0
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-01 12:01:44 -0700

    Add additional calculator rows

--- src/App.js
+++ src/App.js
@@ -11,7 +11,25 @@ class App extends Component {
             <Button>7</Button>
             <Button>8</Button>
             <Button>9</Button>
-            <Button>/</Button>
+            <Button>÷</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>×</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>−</Button>
+          </div>
+          <div className="row">
+            <Button>0</Button>
+            <Button>.</Button>
+            <Button>=</Button>
+            <Button>+</Button>
           </div>
         </div>
       </div>
