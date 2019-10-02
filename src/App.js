e3741a78f09e5871b87585fd651e7269e861a6d0
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-01 21:43:47 -0700

    Add methods for adding to input and calculating

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Button } from './components/Button';
-import { Input } from './components/Input';
-import { ClearButton } from './components/ClearButton';
+import React, { Component } from "react";
+import "./App.css";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import { ClearButton } from "./components/ClearButton";
+import * as math from "mathjs";
 
 class App extends Component {
   constructor(props) {
     /* Have to call super because extending component class */
     super(props);
     this.state = {
-      input: ''
-    }
+      input: ""
+    };
   }
+
+  // Reminder: public class fields syntax. See https://tylermcginnis.com/javascript-private-and-public-class-fields/. Fat arrow functions bind the this variable inside the function call to the this variable of the scope of where the function was defined. So in this case, this would be bound to the current class instance
+  addToInput = val => {
+    // Concatenating onto input with string value from whichever button was clicked
+    this.setState({ input: this.state.input + val });
+  };
+
+  handleEqualsSign = () => {
+    this.setState({ input: math.evaluate(this.state.input) });
+  };
+
   render() {
     return (
       <div className="app">
         <div className="calc-wrapper">
           <Input input={this.state.input}></Input>
           <div className="row">
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button>÷</Button>
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.addToInput}>/</Button>
           </div>
           <div className="row">
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button>×</Button>
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>*</Button>
           </div>
           <div className="row">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>−</Button>
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>−</Button>
           </div>
           <div className="row">
-            <Button>0</Button>
-            <Button>.</Button>
-            <Button>=</Button>
-            <Button>+</Button>
+            <Button handleClick={this.addToInput}>0</Button>
+            <Button handleClick={this.addToInput}>.</Button>
+            <Button handleClick={() => this.handleEqualsSign()}>=</Button>
+            <Button handleClick={this.addToInput}>+</Button>
           </div>
           <div className="row">
-            <ClearButton handleClear={() => this.setState({input: ''})}>
+            {/* Inline functions create a new function on every render; potential performance hit */}
+            <ClearButton handleClear={() => this.setState({ input: "" })}>
               Clear
             </ClearButton>
           </div>
