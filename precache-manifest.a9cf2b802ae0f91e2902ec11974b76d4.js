f13473dac8f111b0ada34345c695103da4a3a821
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-01 22:37:11 -0700

    Updates

---
+++ precache-manifest.a9cf2b802ae0f91e2902ec11974b76d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c869d39dc2235cf8a7c13ea56420fd4",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "ebc0a1207aa4576e709c",
+    "url": "/react-calc/static/css/main.6e7fbfa2.chunk.css"
+  },
+  {
+    "revision": "fc5a586ee7840aa239f5",
+    "url": "/react-calc/static/js/2.27968f25.chunk.js"
+  },
+  {
+    "revision": "ebc0a1207aa4576e709c",
+    "url": "/react-calc/static/js/main.4c1ddc24.chunk.js"
+  },
+  {
+    "revision": "f7a8c937d04f36c13dce",
+    "url": "/react-calc/static/js/runtime-main.b25f44ad.js"
+  }
+]);
